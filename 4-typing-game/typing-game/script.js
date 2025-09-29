a83be363975c1dbd29eb8ca3b13ecbb8d59aea12
Ricardo Oliveira <ricardofoliveira61@gmail.com>
2025-09-29 09:35:01 +0100

    [ADD] Begining of lesson 11

---
+++ 4-typing-game/typing-game/script.js
@@ -0,0 +1,46 @@
+const quotes = [
+    'When you have eliminated the impossible, whatever remains, however improbable, must be the truth.',
+    'There is nothing more deceptive than an obvious fact.',
+    'I ought to know by this time that when a fact appears to be opposed to a long train of deductions it invariably proves to be capable of bearing some other interpretation.',
+    'I never make exceptions. An exception disproves the rule.',
+    'What one man can invent another can discover.',
+    'Nothing clears up a case so much as stating it to another person.',
+    'Education never ends, Watson. It is a series of lessons, with the greatest for the last.',
+]
+
+let words
+let wordIndex
+
+let startTime
+
+const quoteElement = document.getElementById("quote");
+const messageElement = document.getElementById("message");
+const typedValueElement = document.getElementById("typed-value");
+
+document.getElementById("start").addEventListener("click", () =>{
+    
+    const quoteIndex = Math.floor(Math.random() * quotes.length);
+    const quote = quotes[quoteIndex];
+
+    messageElement.innerText = "";
+    typedValueElement.value = "";
+
+    words = quote.split(" ");
+    wordIndex = 0;
+
+    // UI update
+    const spanWords = words.map((word) => {return `<span>${word}</span>`});
+    quoteElement.innerHTML = spanWords.join(" ");
+    quoteElement.childNodes[0].className = 'highlight';
+    
+    typedValueElement.focus();
+    startTime = new Date().getTime();
+})
+
+// add typing logic
+typedValueElement.addEventListener("input",()=>{
+    const currentWord = words[wordIndex];
+    const typedValue = typedValueElement.value;
+
+
+})
